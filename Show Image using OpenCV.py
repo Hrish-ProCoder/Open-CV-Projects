7ec38eaac831a390e02379bd7c2b51b14fe4a151
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-10-24 07:34:33 +0530

    Shows Image

--- Show Image using OpenCV.py
+++ Show Image using OpenCV.py
@@ -1,2 +1,10 @@
 # import opencv
 import cv2
+
+# Location of Image
+# Read in the image
+image = cv2.imread('C:\\Documents\\full_path\\tomatoes.jpg')
+
+# Show the image
+cv2.imshow('Original', image)
+cv2.waitKey(0)
