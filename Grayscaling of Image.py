67c28e5b75657863781acc6bbab909ac12328d90
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-10-25 22:34:46 +0530

    Used cvtColor() function

--- Grayscaling of Image.py
+++ Grayscaling of Image.py
@@ -6,3 +6,7 @@ import cv2
 image = cv2.imread('C:\Desktop\car.jpg')
 cv2.imshow('Original', image)
 cv2.waitKey(0)
+
+# cvtColor() function
+# Grayscale the image
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
