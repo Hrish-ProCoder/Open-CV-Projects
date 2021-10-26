1edbb6663ae099c35a7b374f68bfe3a6008c096c
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-10-27 00:21:23 +0530

    Used cv2.vconcat() to combine images vertically

--- Concatenate Images Vertically.py
+++ Concatenate Images Vertically.py
@@ -2,8 +2,11 @@
 import cv2
 
 # read the images
-img1 = cv2.imread('img1.jpg')
-img2 = cv2.imread('img2.jpg')
+img1 = cv2.imread('dog.jpg')
+img2 = cv2.imread('cat.jpg')
+
+# cv2.vconcat() used to combine images of same width vertically
+img_vrti = cv2.vconcat([img1, img2])
 
 # show the output image
-cv2.imshow('sea_image.jpg', im_v)
+cv2.imshow('pet_img.jpg', img_vrti)
