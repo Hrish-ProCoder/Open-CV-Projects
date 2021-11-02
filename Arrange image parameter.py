ad795262162e4827c2eba70848acd563e14cceb1
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-02 07:07:40 +0530

    Update Arrange image parameter.py

--- Arrange image parameter.py
+++ Arrange image parameter.py
@@ -2,11 +2,11 @@ import tkinter
 from PIL import ImageTk, Image  
 root = tkinter.Tk()  
 
-
+# Application Parameter
 canvas = tkinter.Canvas(root, width = 500, height = 250)
 canvas.pack() 
 
 img = ImageTk.PhotoImage(Image.open("car.jpg"))  
 
-
+# Image Parameter
 canvas.create_image(135, 20, anchor = NW, image = img) 
