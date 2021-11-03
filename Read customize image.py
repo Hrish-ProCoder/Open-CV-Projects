3a554990d2039d9e2924f3d8f7b65c89aec583c5
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-03 06:35:34 +0530

    Update Read customize image.py

--- Read customize image.py
+++ Read customize image.py
@@ -3,6 +3,7 @@ from PIL import ImageTk, Image
 
 root = tkinter.Tk()  
 
-canvas = tkinter.Canvas(root, width = 500, 
-                        height = 250)  
+canvas = tkinter.Canvas(root, width = 500, height = 250)  
 canvas.pack() 
+
+img = ImageTk.PhotoImage(Image.open("cars.jpg"))
