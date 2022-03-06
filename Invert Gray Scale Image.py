cd387b320221744c324dff342653906a29720c2c
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2022-03-06 21:44:15 +0530

    Update Invert Gray Scale Image.py

--- Invert Gray Scale Image.py
+++ Invert Gray Scale Image.py
@@ -4,8 +4,6 @@ import cv2
 # Read the image
 # Load the input image
 image = cv2.imread('C:\Desktop\car.jpg')
-cv2.imshow('Original', image)
-cv2.waitKey(0)
 
 # cvtColor() function
 # Greyscale the image
@@ -14,6 +12,7 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Invert the image
 Inverted_grayimage = 255 - gray_image
 
+# Show image
 cv2.imshow('Original Image', inverted_gray_image)
 cv2.imshow('New Image', inverted_gray_image)
 cv2.waitKey(0) 
