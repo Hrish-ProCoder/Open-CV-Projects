b958b295773eacb27dbf939c9049f02b27ad3b12
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2022-03-06 21:37:30 +0530

    Update Invert Grey Scale Image.py

--- Invert Grey Scale Image.py
+++ Invert Grey Scale Image.py
@@ -7,3 +7,7 @@ image = cv2.imread('C:\Desktop\car.jpg')
 cv2.imshow('Original', image)
 cv2.waitKey(0)
 
+# cvtColor() function
+# Grayscale the image
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
