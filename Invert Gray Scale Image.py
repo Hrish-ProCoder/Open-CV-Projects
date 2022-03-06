86ced580a241e91acca700a80ac0783d599979ff
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2022-03-06 21:42:37 +0530

    Update and rename Invert Grey Scale Image.py to Invert Gray Scale Image.py

--- Invert Gray Scale Image.py
+++ Invert Gray Scale Image.py
@@ -8,10 +8,14 @@ cv2.imshow('Original', image)
 cv2.waitKey(0)
 
 # cvtColor() function
-# Grayscale the image
+# Greyscale the image
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('Grayscale', gray_image)
+# Invert the image
+Inverted_grayimage = 255 - gray_image
+
+cv2.imshow('Original Image', inverted_gray_image)
+cv2.imshow('New Image', inverted_gray_image)
 cv2.waitKey(0) 
 
 cv2.destroyAllWindows()
