59bb47389e1c1f384ee77de43d2ecf9f2ec53d64
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2022-03-12 13:57:48 +0530

    Update Invert Blur Image.py

--- Invert Blur Image.py
+++ Invert Blur Image.py
@@ -3,6 +3,9 @@ import cv2
 img = cv2.imread('watch.jpg')
 
 blur_image = cv2.blur(img,(21,21))
+
+inverted_blurred_image = 255 - blur_image
+
 cv2.imshow('blurred image',blur_image)
 
 cv2.waitKey(0)
