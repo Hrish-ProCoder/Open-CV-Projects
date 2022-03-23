44e9165781a99917c0e60fdf23c3a57766ca07f6
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2022-03-23 17:47:40 +0530

    Update Analyze image using Histogram.py

--- Analyze image using Histogram.py
+++ Analyze image using Histogram.py
@@ -5,7 +5,7 @@ plt.hist(n_img.ravel(), bins=256, range=(0.0, 1.0), fc='k', ec='k')
 
 cv2.calcHist(images, channels, mask, histSize, ranges[, hist[, accumulate]])
 
-
+# Eg 0-256
 img = cv2.imread('smrt.jpg',0)
 
 histg = cv2.calcHist([img],[0],None,[256],[0,256])
