f7573a76dad95940a5ecfc25f1fce7ea767282be
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2022-03-23 17:46:48 +0530

    Update Analyze image using Histogram.py

--- Analyze image using Histogram.py
+++ Analyze image using Histogram.py
@@ -2,3 +2,5 @@ import matplotlib.pyplot as plt
 img = plt.imread('watch.jpg')
 
 plt.hist(n_img.ravel(), bins=256, range=(0.0, 1.0), fc='k', ec='k')
+
+cv2.calcHist(images, channels, mask, histSize, ranges[, hist[, accumulate]])
