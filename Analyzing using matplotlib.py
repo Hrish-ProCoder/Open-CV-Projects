089f34133b3c58f487dff986fd121899efecc0f1
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2022-03-24 07:22:15 +0530

    Update Analyzing using matplotlib.py

--- Analyzing using matplotlib.py
+++ Analyzing using matplotlib.py
@@ -1,3 +1,4 @@
+# find histogram of an image
 import cv2
 from matplotlib import pyplot as plt
 image = cv2.imread('watch.jpg',0)
@@ -7,3 +8,8 @@ histo = cv2.calcHist([image],[0],None,[256],[0,256])
 plt.plot(histo)
 
 plt.show()
+
+
+
+# alternate way
+
