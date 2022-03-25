45683773d95ff810d3d760d7a92ea390a6221177
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2022-03-25 07:18:58 +0530

    Update Border the image.py

--- Border the image.py
+++ Border the image.py
@@ -6,3 +6,5 @@ window_name = 'Image'
  
 # Using cv2.copyMakeBorder() method
 image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, None, value = 0)
+ 
+cv2.imshow(window_name, image)
