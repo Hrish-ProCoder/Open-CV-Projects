26fb7c72b031c709a565c0eb43f9de58d00df7ca
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2022-03-26 07:28:44 +0530

    Update Histogram Equilization.py

--- Histogram Equilization.py
+++ Histogram Equilization.py
@@ -3,7 +3,10 @@ import numpy as np
 
 image = cv2.imread('C:/HK/Downloads/watch.jpg', 0)
 
-equ = cv2.equalizeHist(image)
+equ = cv2.equalizeHist(img)
 
-res = np.hstack((img, equ))
+res = np.hstack((image, equ))
 
+cv2.imshow(\'img\', res)
+
+cv2.waitKey(0)
