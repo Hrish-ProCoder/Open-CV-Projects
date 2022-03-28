20bbbc83f370ed483e2d9a85b0d3decd02a82dfb
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2022-03-28 07:10:29 +0530

    Update Rotate image.py

--- Rotate image.py
+++ Rotate image.py
@@ -27,3 +27,5 @@ dest = r'C:\Users\HK\Download\watch.jpg'
 src = cv2.imread(dest)
 window_name = 'Image'
 image = cv2.rotate(src, cv2.ROTATE_90_COUNTERCLOCKWISE)
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
